a46efb16b42e3bdd808f2949b2e471404dbf6894
chursin.pp <chursin.pp@ccnpp.ru>
2023-10-24 23:30:06 +0700

    lr4_full_with_photo_termo_resistors

--- lr4_photometer/lr4_photometer.ino
+++ lr4_photometer/lr4_photometer.ino
@@ -1,11 +1,28 @@
 bool flag = true;
 
+const int matrixPin12 = 10;
+const int matrixPin11 = 11;
+const int matrixPin10 = 12;
+const int matrixPin9 = 13;
+
+const int matrixPin1 = 2;
+const int matrixPin2 = 3;
+const int matrixPin3 = 4;
+const int matrixPin4 = 5;
+
 void setup() {
-  pinMode (12, OUTPUT);
-  pinMode (11, OUTPUT);
-  pinMode (10, OUTPUT);
-  pinMode (9 , OUTPUT);
-  pinMode (A0, INPUT);
+  pinMode(9, OUTPUT);
+  pinMode(A0, INPUT);
+
+  pinMode(matrixPin1, OUTPUT);
+  pinMode(matrixPin2, OUTPUT);
+  pinMode(matrixPin3, OUTPUT);
+  pinMode(matrixPin4, OUTPUT);
+
+  pinMode(matrixPin9, OUTPUT);
+  pinMode(matrixPin10, OUTPUT);
+  pinMode(matrixPin11, OUTPUT);
+  pinMode(matrixPin12, OUTPUT);
 
   Serial.begin(9600);
 }
@@ -14,22 +31,34 @@ void loop() {
 
   int val = analogRead(A0);
   Serial.println(val);
-  
-  if (val >= 100 && flag) {
-    
-    digitalWrite (12, LOW);
-    digitalWrite (11, LOW);
-    digitalWrite (10, LOW);
-    digitalWrite (9, LOW);
+
+  if (val < 430 && flag) {
+
+    digitalWrite(9, LOW);
+    digitalWrite(matrixPin1, LOW);
+    digitalWrite(matrixPin2, LOW);
+    digitalWrite(matrixPin3, LOW);
+    digitalWrite(matrixPin4, LOW);
+
+    digitalWrite(matrixPin9, LOW);
+    digitalWrite(matrixPin10, LOW);
+    digitalWrite(matrixPin11, LOW);
+    digitalWrite(matrixPin12, LOW);
+
     flag = false;
-  } 
-  else 
-  if (val < 100 && !flag) {
-    
-    digitalWrite (12, HIGH);
-    digitalWrite (11, HIGH);
-    digitalWrite (10, HIGH);
-    digitalWrite (9, HIGH);
+  } else if (val >= 430 && !flag) {
+
+    digitalWrite(matrixPin1, HIGH);
+    digitalWrite(matrixPin2, HIGH);
+    digitalWrite(matrixPin3, HIGH);
+    digitalWrite(matrixPin4, HIGH);
+
+    digitalWrite(matrixPin9, HIGH);
+    digitalWrite(matrixPin10, HIGH);
+    digitalWrite(matrixPin11, HIGH);
+    digitalWrite(matrixPin12, HIGH);
+
+    digitalWrite(9, HIGH);
     flag = true;
   }
 }
